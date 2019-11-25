b3bc6c2db43cbbf4fc33f62954874621510341bb
phoenixmcknight <phoenixmcknight@gmail.com>
2019-11-25 15:10:43 -0500

    moves create user function from login VC to profileVC

--- FireBaseProject/ViewControllers/Login/LoginViewController.swift
+++ FireBaseProject/ViewControllers/Login/LoginViewController.swift
@@ -16,7 +16,8 @@ class LoginViewController: UIViewController {
        lazy var logoLabel: UILabel = {
               let label = UILabel()
               label.numberOfLines = 0
-              label.text = "Fave Spots"
+              label.text = "PursuitGram"
+        label.adjustsFontSizeToFitWidth = true
               label.font = UIFont(name: "Verdana-Bold", size: 60)
               label.textColor = UIColor(red: 255/255, green: 86/255, blue: 0/255, alpha: 1.0)
               label.backgroundColor = .clear
